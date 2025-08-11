15a64ffed7571736d99fc440135409dcec8f277a
Jack Moroney <jack@justus.co>
2025-08-11 09:44:57 +0100

    Refreshing git commands

--- index.php
+++ index.php
@@ -17,10 +17,104 @@
 
     <div class="body container">
 
-
-        <div class="row container">
-            <h1>Welcome to Codelis</h1>
-            <p>This is the home page of Codelis. Explore our features and services.</p>
+        <div class="row container my-5">
+            <h1>Welcome to Codelis <small>For testing bootstrap</small></h1>
         </div>
+
+        <h2>Columns</h2>
+
+        <div class="row my-5">
+            <div class="col-md-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Mauris elementum magna ut tortor fermentum, a vehicula libero ultricies. Nam vel vehicula eros, sed
+                vulputate mauris.
+                Proin at magna vitae enim consequat finibus. Maecenas in rhoncus purus.
+                Nulla congue molestie interdum. Duis vel dictum risus. Aenean placerat arcu eget suscipit elementum.
+            </div>
+            <div class="col-md-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Mauris elementum magna ut tortor fermentum, a vehicula libero ultricies. Nam vel vehicula eros, sed
+                vulputate mauris.
+                Proin at magna vitae enim consequat finibus. Maecenas in rhoncus purus.
+                Nulla congue molestie interdum. Duis vel dictum risus. Aenean placerat arcu eget suscipit elementum.
+            </div>
+            <div class="col-md-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Mauris elementum magna ut tortor fermentum, a vehicula libero ultricies. Nam vel vehicula eros, sed
+                vulputate mauris.
+                Proin at magna vitae enim consequat finibus. Maecenas in rhoncus purus.
+                Nulla congue molestie interdum. Duis vel dictum risus. Aenean placerat arcu eget suscipit elementum.
+            </div>
+        </div>
+
+        <div class="row my-5">
+            <div class="col-sm-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Mauris elementum magna ut tortor fermentum, a vehicula libero ultricies. Nam vel vehicula eros, sed
+                vulputate mauris.
+                Proin at magna vitae enim consequat finibus. Maecenas in rhoncus purus.
+                Nulla congue molestie interdum. Duis vel dictum risus. Aenean placerat arcu eget suscipit elementum.
+            </div>
+            <div class="col-sm-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Mauris elementum magna ut tortor fermentum, a vehicula libero ultricies. Nam vel vehicula eros, sed
+                vulputate mauris.
+                Proin at magna vitae enim consequat finibus. Maecenas in rhoncus purus.
+                Nulla congue molestie interdum. Duis vel dictum risus. Aenean placerat arcu eget suscipit elementum.
+            </div>
+            <div class="col-sm-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Mauris elementum magna ut tortor fermentum, a vehicula libero ultricies. Nam vel vehicula eros, sed
+                vulputate mauris.
+                Proin at magna vitae enim consequat finibus. Maecenas in rhoncus purus.
+                Nulla congue molestie interdum. Duis vel dictum risus. Aenean placerat arcu eget suscipit elementum.
+            </div>
+        </div>
+
+        <div class="row my-5">
+            <div class="col-sm-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Mauris elementum magna ut tortor fermentum, a vehicula libero ultricies. Nam vel vehicula eros, sed
+                vulputate mauris.
+                Proin at magna vitae enim consequat finibus. Maecenas in rhoncus purus.
+                Nulla congue molestie interdum. Duis vel dictum risus. Aenean placerat arcu eget suscipit elementum.
+            </div>
+            <div class="col-sm-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Mauris elementum magna ut tortor fermentum, a vehicula libero ultricies. Nam vel vehicula eros, sed
+                vulputate mauris.
+                Proin at magna vitae enim consequat finibus. Maecenas in rhoncus purus.
+                Nulla congue molestie interdum. Duis vel dictum risus. Aenean placerat arcu eget suscipit elementum.
+            </div>
+        </div>
+
+        <h2>Tables</h2>
+        <div class="row my-5">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First</th>
+                        <th>Last</th>
+                        <th>Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th>2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th>3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+        </div>
+
+
+
+        </table>
     </div>
 </body>
